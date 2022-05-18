ba4a4dc68695bd30eb0f13e1716936a72b7b3808
wasiulhaque <wasiulhq@gmail.com>
2022-05-18 10:52:24 +0600

    Implemented Search Bar, autocomplete doesn't work

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,8 +3,14 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import tw from 'twrnc';
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import {GOOGLE_MAPS_APIKEY} from "@env";
+import {useDispatch} from "react-redux";
+import {setDestination, setOrigin} from "../slices/navSlice";
+
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
     <View style={tw`p-5`}>
@@ -19,6 +25,35 @@ const HomeScreen = () => {
         source={{
             uri: "https://logos-world.net/wp-content/uploads/2020/05/Uber-Logo.png"
         }}
+      />
+
+      <GooglePlacesAutocomplete
+        placeholder='Where From?'
+        styles={{
+          container: {
+            flex: 0,
+          },
+          textInput: {
+            fontSize: 18,
+          },
+        }}
+        onPress={(data, details = null)=> {
+          dispatch(setOrigin({
+            location: details.geometry.location,
+            description: data.description
+          }))
+
+          dispatch(setDestination(null));
+        }}
+        fetchDetails={true}
+        returnKeyType={"search"}
+        query={{
+          key: GOOGLE_MAPS_APIKEY,
+          language:'en'
+        }}
+
+        nearbyPlacesAPI='GooglePlacesSearch'
+        debounce={400}
       />
 
       <NavOptions/>
