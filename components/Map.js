9781f7751e1d8cff70a0954d39893b7f98511366
wasiulhaque <wasiulhq@gmail.com>
2022-05-18 15:14:22 +0600

    NavigateCard completed

--- components/Map.js
+++ components/Map.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import MapView from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps'
 import tw from 'twrnc'
 import { useSelector } from 'react-redux'
 import { selectOrigin } from '../slices/navSlice'
@@ -20,6 +20,17 @@ const Map = () => {
           longitudeDelta: 0.005,
         }}
       >
+        {origin?.location &&(
+          <Marker
+            coordinate={{
+              latitude: origin.location.lat,
+              longitude: origin.location.lng,
+            }}
+            title="Starting Point"
+            description={origin.description}
+            identifier="origin"
+          />
+        )}
       </MapView>
     );
   };
---
+++ components/NavigateCard.js
@@ -0,0 +1,65 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import tw from 'twrnc'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import {GOOGLE_MAPS_APIKEY} from "@env";
+import { useDispatch } from 'react-redux';
+import { setDestination } from '../slices/navSlice';
+import { useNavigation } from '@react-navigation/native';
+
+const NavigateCard = () => {
+const dispatch = useDispatch();
+const navigation = useNavigation();
+    
+  return (
+    <SafeAreaView style={tw`bg-white flex-1`}>
+        <Text style={tw`text-center py-1 text-xl`}>Good Morning</Text>
+        <View style={tw`flex-shrink`}>
+            <View>
+                <GooglePlacesAutocomplete
+                placeholder='Where To?'
+                styles={toInputBoxStyles}
+                fetchDetails={true}
+                enablePoweredByContainer={false}
+                returnKeyType={"search"}
+                minLength={2}
+                onPress={(data, details = null)=>{
+                    dispatch(setDestination({
+                        location: details.geometry.location,
+                        description: data.description,
+                    }));
+
+                    navigation.navigate("RideOptionsCard");
+                }}
+                query={{
+                    key: GOOGLE_MAPS_APIKEY,
+                    language: "en",
+                }}
+                nearbyPlacesAPI='GooglePlacesSearch'
+                debounce={400}
+                />
+            </View>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default NavigateCard
+
+const toInputBoxStyles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        paddingTop: 20,
+        flex: 0,
+    },
+    textInput: {
+        backgroundColor: "#DDDDDF",
+        borderRadius: 10,
+        fontSize: 18,
+    },
+    textInputContainer: {
+        paddingHorizontal: 20,
+        paddingBottom: 0,
+    },
+});
